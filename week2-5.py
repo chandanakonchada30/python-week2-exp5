78910a2e1f238d8b29eb1771b0b8ff791568c95d
chandanakonchada30 <24331a05e2@mvgrce.edu.in>
2025-12-14 10:04:06 +0530

    Add files via upload

---
+++ week2-5.py
@@ -0,0 +1,6 @@
+#2433A05E2
+#TO FIND THE AREA OF TRIANGLE
+base=float(input("Enter base of triangle: "))
+height=float(input("Enter height of triangle: "))
+area=0.5*base*height
+print("Area of triangle is:", area)
